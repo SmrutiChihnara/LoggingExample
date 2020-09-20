34bc2845e18dff5a6b4a3d7ed528229907e3cb9b
Smruti <chihnara93@gmail.com>
2020-09-20 19:36:49 +0530

    first commit

---
+++ src/main/java/com/example/LoggingExample/LoggingExampleApplication.java
@@ -0,0 +1,29 @@
+package com.example.LoggingExample;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class LoggingExampleApplication {
+	Logger log= LoggerFactory.getLogger (LoggingExampleApplication.class);
+@GetMapping("get/{name}")
+	public String getLogheck(@PathVariable String name)
+	{
+		log.debug ("Logger debung",name);
+		log.info ("info Test");
+		return "HelloLogger";
+	}
+
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LoggingExampleApplication.class, args);
+	}
+
+}
